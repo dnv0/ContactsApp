c74f8da14b95b74595cb4874cff06e28467c1f95
dnv0 <60728565+dnv0@users.noreply.github.com>
2020-02-12 18:05:28 +0900

    Реализован listBox с выводом информации о текущем контакте

--- ContactsApp/ContactsAppUI/MainForm.Designer.cs
+++ ContactsApp/ContactsAppUI/MainForm.Designer.cs
@@ -28,140 +28,237 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.jDragControl1 = new JDragControl.JDragControl(this.components);
+            this.mainMenu1 = new System.Windows.Forms.MainMenu(this.components);
+            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            this.menuItem2 = new System.Windows.Forms.MenuItem();
+            this.menuItem3 = new System.Windows.Forms.MenuItem();
+            this.menuItem4 = new System.Windows.Forms.MenuItem();
+            this.menuItem5 = new System.Windows.Forms.MenuItem();
+            this.menuItem6 = new System.Windows.Forms.MenuItem();
+            this.menuItem7 = new System.Windows.Forms.MenuItem();
+            this.menuItem8 = new System.Windows.Forms.MenuItem();
+            this.textBoxSurename = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxIdvkcom = new System.Windows.Forms.TextBox();
+            this.textBoxPhone = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 58);
+            this.label1.Location = new System.Drawing.Point(311, 24);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(52, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Surname:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(24, 83);
+            this.label2.Location = new System.Drawing.Point(325, 55);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(38, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Text = "Name:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(24, 106);
+            this.label3.Location = new System.Drawing.Point(315, 84);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(48, 13);
             this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.label3.Text = "Birthday:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(24, 129);
+            this.label4.Location = new System.Drawing.Point(328, 114);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(35, 13);
             this.label4.TabIndex = 3;
-            this.label4.Text = "label4";
+            this.label4.Text = "Email:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(24, 155);
+            this.label5.Location = new System.Drawing.Point(303, 144);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.Size = new System.Drawing.Size(60, 13);
             this.label5.TabIndex = 4;
-            this.label5.Text = "label5";
+            this.label5.Text = "ID Vk.com:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(24, 182);
+            this.label6.Location = new System.Drawing.Point(322, 176);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.Size = new System.Drawing.Size(41, 13);
             this.label6.TabIndex = 5;
-            this.label6.Text = "label6";
+            this.label6.Text = "Phone:";
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem1,
+            this.menuItem3,
+            this.menuItem7});
+            // 
+            // menuItem1
+            // 
+            this.menuItem1.Index = 0;
+            this.menuItem1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem2});
+            this.menuItem1.Text = "File";
+            // 
+            // menuItem2
+            // 
+            this.menuItem2.Index = 0;
+            this.menuItem2.Text = "Exit";
+            // 
+            // menuItem3
+            // 
+            this.menuItem3.Index = 1;
+            this.menuItem3.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem4,
+            this.menuItem5,
+            this.menuItem6});
+            this.menuItem3.Text = "Edit";
+            // 
+            // menuItem4
+            // 
+            this.menuItem4.Index = 0;
+            this.menuItem4.Text = "Add Contact";
+            // 
+            // menuItem5
+            // 
+            this.menuItem5.Index = 1;
+            this.menuItem5.Text = "Edit Contact";
+            // 
+            // menuItem6
+            // 
+            this.menuItem6.Index = 2;
+            this.menuItem6.Text = "Remove Contact";
+            // 
+            // menuItem7
+            // 
+            this.menuItem7.Index = 2;
+            this.menuItem7.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem8});
+            this.menuItem7.Text = "Help";
+            // 
+            // menuItem8
+            // 
+            this.menuItem8.Index = 0;
+            this.menuItem8.Text = "About";
+            // 
+            // textBoxSurename
+            // 
+            this.textBoxSurename.Location = new System.Drawing.Point(369, 21);
+            this.textBoxSurename.Name = "textBoxSurename";
+            this.textBoxSurename.Size = new System.Drawing.Size(425, 20);
+            this.textBoxSurename.TabIndex = 12;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(369, 52);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(425, 20);
+            this.textBoxName.TabIndex = 13;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(369, 111);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(425, 20);
+            this.textBoxEmail.TabIndex = 14;
+            // 
+            // textBoxIdvkcom
+            // 
+            this.textBoxIdvkcom.Location = new System.Drawing.Point(369, 141);
+            this.textBoxIdvkcom.Name = "textBoxIdvkcom";
+            this.textBoxIdvkcom.Size = new System.Drawing.Size(425, 20);
+            this.textBoxIdvkcom.TabIndex = 15;
+            // 
+            // textBoxPhone
+            // 
+            this.textBoxPhone.Location = new System.Drawing.Point(369, 173);
+            this.textBoxPhone.Name = "textBoxPhone";
+            this.textBoxPhone.Size = new System.Drawing.Size(425, 20);
+            this.textBoxPhone.TabIndex = 16;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(369, 81);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(425, 20);
+            this.dateTimePicker1.TabIndex = 17;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(24, 52);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(273, 446);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(60, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(237, 20);
+            this.textBox1.TabIndex = 18;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(22, 24);
+            this.label7.Location = new System.Drawing.Point(24, 24);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(35, 13);
-            this.label7.TabIndex = 6;
-            this.label7.Text = "label7";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(27, 240);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(66, 23);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "<-";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(111, 240);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "->";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(27, 269);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(66, 23);
-            this.button3.TabIndex = 9;
-            this.button3.Text = "Add Contact";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(111, 269);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 10;
-            this.button4.Text = "Load File";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.label7.Size = new System.Drawing.Size(30, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Find:";
             // 
             // MainForm
             // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(825, 534);
             this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBoxPhone);
+            this.Controls.Add(this.textBoxIdvkcom);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBoxSurename);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Menu = this.mainMenu1;
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "ContactsApp";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -175,11 +272,25 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private JDragControl.JDragControl jDragControl1;
+        private System.Windows.Forms.MainMenu mainMenu1;
+        private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.MenuItem menuItem2;
+        private System.Windows.Forms.MenuItem menuItem3;
+        private System.Windows.Forms.MenuItem menuItem4;
+        private System.Windows.Forms.MenuItem menuItem5;
+        private System.Windows.Forms.MenuItem menuItem6;
+        private System.Windows.Forms.MenuItem menuItem7;
+        private System.Windows.Forms.MenuItem menuItem8;
+        private System.Windows.Forms.TextBox textBoxSurename;
+        private System.Windows.Forms.TextBox textBoxIdvkcom;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBoxPhone;
+        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- ContactsApp/ContactsAppUI/MainForm.cs
+++ ContactsApp/ContactsAppUI/MainForm.cs
@@ -13,105 +13,36 @@ namespace ContactsApp
 {
     public partial class MainForm : Form
     {
-        private Project project = new Project();
-        private int n = 0;
+        private Project _project;
         public MainForm()
         {
             InitializeComponent();
 
-            Contact contact1 = new Contact
-            {
-                Surname = "Reeves",
-                Name = "Keeanu",
-                DateOfBirth = new DateTime(1990, 02, 26),
-                Email = "123@mail.ru",
-                IDVkcom = "1234567",
-                phoneNumber = {Number = "79991234567"}
-            };
+            // Загрузка файла с контактами
+            //
+            _project = ProjectManager.LoadFromFile(ProjectManager.stringMyDocumentsPath);
 
-
-            Contact contact2 = (Contact)contact1.Clone();
-
-            Contact contact3 = new Contact
-            {
-                Surname = "Yolo",
-                Name = "Volvo",
-                DateOfBirth = new DateTime(1993, 05, 06),
-                Email = "yolo@volvo.com",
-                IDVkcom = "7654321",
-                phoneNumber = { Number = "79993332211" }
-            };
-
-            project.ContactsList.Add(contact1);
-            project.ContactsList.Add(contact2);
-            project.ContactsList.Add(contact3);
-
-            
-                label1.Text = project.ContactsList[n].Surname;
-                label2.Text = project.ContactsList[n].Name;
-                label3.Text = project.ContactsList[n].DateOfBirth.ToString();
-                label4.Text = project.ContactsList[n].Email;
-                label5.Text = project.ContactsList[n].IDVkcom;
-                label6.Text = project.ContactsList[n].phoneNumber.Number;
-
-                label7.Text = "Contact Number: " + n;
-            
-           ProjectManager.SaveToFile(project, ProjectManager.stringMyDocumentsPath);
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            
-            if (project.ContactsList.Count-1 != n)
-            {
-                n++;
-            }
-
-            label1.Text = project.ContactsList[n].Surname;
-            label2.Text = project.ContactsList[n].Name;
-            label3.Text = project.ContactsList[n].DateOfBirth.ToString();
-            label4.Text = project.ContactsList[n].Email;
-            label5.Text = project.ContactsList[n].IDVkcom;
-            label6.Text = project.ContactsList[n].phoneNumber.Number;
-
-            label7.Text = "Contact Number: " + n;
-        }
-
-        private void button1_Click_1(object sender, EventArgs e)
-        {
-            if (n != 0)
-            {
-                n--;
-            }
-            label1.Text = project.ContactsList[n].Surname;
-            label2.Text = project.ContactsList[n].Name;
-            label3.Text = project.ContactsList[n].DateOfBirth.ToString();
-            label4.Text = project.ContactsList[n].Email;
-            label5.Text = project.ContactsList[n].IDVkcom;
-            label6.Text = project.ContactsList[n].phoneNumber.Number;
-
-            label7.Text = "Contact Number: " + n;
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            Contact contact4 = new Contact
-            {
-                Surname = "Unsaved",
-                Name = "File",
-                DateOfBirth = new DateTime(1995, 05, 06),
-                Email = "unsaved@file.com",
-                IDVkcom = "7654321",
-                phoneNumber = { Number = "79993332211" }
-            };
-            project.ContactsList.Add(contact4);
-
+            // Загрузка в listBox контактов из ContactList
+            //
+            listBox1.DataSource = _project.ContactsList;
+            listBox1.DisplayMember = "Surname";
 
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        private void MainForm_Load(object sender, EventArgs e)
         {
-           project = ProjectManager.LoadFromFile(ProjectManager.stringMyDocumentsPath);
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Contact c = (Contact)listBox1.SelectedItem;
+            textBoxSurename.Text = c.Surname;
+            textBoxName.Text = c.Name;
+            textBoxEmail.Text = c.Email;
+            textBoxIdvkcom.Text = c.IDVkcom;
+            textBoxPhone.Text = c.phoneNumber.Number;
+            dateTimePicker1.Value = c.DateOfBirth;
         }
     }
 }
